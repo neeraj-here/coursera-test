d6daf2b6bc8d4546e4f0cb4d5ae70b8872d5fc4c
neeraj-here <19bcs1298@gmail.com>
2020-07-20 13:19:45 +0530

    updtation

--- learn-js/js/script.js
+++ learn-js/js/script.js
@@ -13,6 +13,7 @@ a();
 var x=4,y=5;
 if(x==y)
 	console.log("equal");*/
+/*
 var company=new Object();
 company.name="facebook";
 console.log(company.name);
@@ -24,3 +25,33 @@ console.log(company.ceo.firstname);
 var quote="Our Motto";
 company[quote]="we make people feel close to each other";
 console.log(company[quote]);
+var facebook={
+	name: "facebook",
+	ceo: {
+		firstname: "Mark",
+		favcolor: "Blue"
+	},
+	"stock price": 110
+};
+console.log(facebook);
+function makemultiplier(multiplier)
+{
+	var myfunc= function(x){
+		return multiplier*x;
+	};
+	return myfunc;
+}
+var result= makemultiplier(3);
+console.log(result(10));
+var doubleme=makemultiplier(2);
+console.log(doubleme(100));*/
+//function constructors
+function Circle(radius){
+	this.radius=radius;
+}
+Circle.prototype.Getarea = function() {
+	return Math.PI * Math.pow(this.radius,2);
+};
+var mycircle=new Circle(10);
+console.log(mycircle.Getarea());
+
